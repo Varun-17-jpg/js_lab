7d5037e456870c1f9f908dd70dca0f45c919d822
Varun-17-jpg <varunpandey171@gmail.com>
2025-07-30 04:38:53 -0400

    message

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -17,7 +17,7 @@ console.log(globalConst); // Output: "I'm a global constant"
 
 //Block Scope
 console.log(blockVar);
-console.log(blockLet);
+//console.log(blockLet);//Uncaught reference error
 
 function show(){
 var functionVar = "I'm a block-scoped var";
@@ -26,9 +26,6 @@ const functionConst = "I'm a block-scoped const";
 }
 show();
 
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
-
-//console.log(blockVar);
-//console.log(blockLet);
+//console.log(functionVar); // Throws ReferenceError
+//console.log(functionLet); // Throws ReferenceError
+//console.log(functionConst); // Throws ReferenceError
